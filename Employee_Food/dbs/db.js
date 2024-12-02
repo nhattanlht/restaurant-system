d5b0c3da12380a2a1aefc65b73d639a16af6edbd
nasm1023 <namtl432@gmail.com>
2024-12-02 11:32:57 +0700

    add file cart to employee

--- Employee_Food/dbs/db.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const config = require('../configs/knexfile.js')
-
-const db = knex(config);
-
-module.exports = db;
--- Employee_Food/routes/search_filter.route.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const FilterController=require('../controllers/search_filter_controller.js');
-router.get('/filter',FilterController.renderFoods)
-
-module.exports=router;
